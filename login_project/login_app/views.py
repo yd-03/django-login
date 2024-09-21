491eabf6f4a5f6ec20dedc42065ce863bff093c5
yd-03 <3nu4ydye57@gmail.com>
2024-09-21 06:56:48 +0000

    feat: ユーザーアカウントの登録、ログイン、ログアウト、ユーザー情報表示の関数を追加

--- login_project/login_app/views.py
+++ login_project/login_app/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+def signup_view(request):
+    # ユーザーアカウントの登録
+    pass
 
-# Create your views here.
+
+def login_view(request):
+    # ユーザーのログイン
+    pass
+
+
+def logout_view(request):
+    # ユーザーのログアウト
+    pass
+
+
+def user_view(request):
+    # ユーザーの情報を表示
+    pass
+
+
+def other_view(request):
+    # 他のユーザーの情報を表示
+    pass
